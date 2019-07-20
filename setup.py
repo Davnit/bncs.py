e71826b2ee3353f69c4826802700c518a85e4aa0
Davnit <davnit31@gmail.com>
2019-07-19 21:34:36 -0400

    Update setup.py for new dependencies and packages

--- setup.py
+++ setup.py
@@ -5,10 +5,12 @@ from setuptools import setup
 
 setup(
     name='bncs.py',
-    version='0.1',
+    version='0.2',
     description='Python library for classic Battle.net client development.',
     author='Davnit',
     author_email='david@davnit.net',
     url='https://github.com/Davnit/bncs.py',
-    packages=['bncs', 'bnls']
+    packages=['bncs', 'bnftp', 'bnls', 'botnet', 'capi'],
+    install_requires=['pefile', 'signify'],
+    python_requires='>=3.7'
 )
