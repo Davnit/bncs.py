4495fbc28f5687efb36d924a38c9f015bfa97269
Davnit <davnit31@gmail.com>
2020-11-28 20:27:45 -0500

    Update package version and required python version
    Now requires 3.8.

--- setup.py
+++ setup.py
@@ -5,12 +5,12 @@ from setuptools import setup, find_packages
 
 setup(
     name='bncs.py',
-    version='0.2',
+    version='0.3',
     description='Python library for classic Battle.net client development.',
     author='Davnit',
     author_email='david@davnit.net',
     url='https://github.com/Davnit/bncs.py',
     packages=find_packages(),
     install_requires=['pefile', 'signify'],
-    python_requires='>=3.7'
+    python_requires='>=3.8'
 )
