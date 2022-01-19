f96736b8e39795a75513b625638f53bcfbeab36b
Davnit <Davnit@users.noreply.github.com>
2022-01-18 23:29:55 -0500

    Have CRev test script compare with BNLS

--- scripts/crev_test.py
+++ scripts/crev_test.py
@@ -8,12 +8,6 @@ from bncs import LocalHashingProvider, BncsProduct
 from bnls import BnlsClient
 
 
-def print_result(t):
-    r, d = t.result()
-    print(f"Results: {r}")
-    print(f"Time taken: {d} seconds")
-
-
 async def main(args):
     logging.basicConfig(level=logging.DEBUG)
 
@@ -24,25 +18,29 @@ async def main(args):
         seed = args.seed.encode('ascii')
 
     product = BncsProduct.get(args.product)
-    local = LocalHashingProvider(args.hashes)
-    await local.preload([('IX86', product.code)], version=args.archive)
 
-    # remote = BnlsClient()
-    # await remote.connect()
+    remote = BnlsClient()
+    local = LocalHashingProvider(args.hashes)
+
+    await asyncio.gather(
+        local.preload([('IX86', product.code)], version=args.archive),
+        remote.connect()
+    )
 
     async def run_test(src, tag):
         print(f"Starting {tag} test on {product.code} with archive {args.archive}")
         start = time.perf_counter()
         result = await src.check_version(product.code, args.archive, seed)
         duration = (time.perf_counter() - start)
-        return result, duration
+        print(f"{tag} {result} (time: {duration:.3f}s)")
 
-    task = asyncio.create_task(run_test(local, "LOCAL"))
-    task.add_done_callback(print_result)
-    await task
+    remote_test = asyncio.create_task(run_test(remote, "REMOTE"))
+    local_test = asyncio.create_task(run_test(local, "LOCAL"))
 
-    # remote.disconnect("done")
-    # await remote.wait_closed()
+    await asyncio.gather(remote_test, local_test)
+
+    remote.disconnect("done")
+    await remote.wait_closed()
 
 
 if __name__ == "__main__":
